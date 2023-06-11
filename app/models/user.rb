ea74054842978867c7596f855b4148b6009528d1
fukumochi-0110 <fukumoto08@gmail.com>
2023-06-12 01:58:07 +0900

    パスワード、名前、ナマエの正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname,:first_name, :last_name, :first_name_kana	, :last_name_kana, :birthday ,presence: true
+  validates :email, unique: true
+  validates :nickname, :birthday ,presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' 
+
+  validates :first_name, :last_name, presence: true
+
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/.freeze
+
+  validates_format_of :last_name, with: NAME_REGEX, message: 'is invalid. Input full-width character' 
+  validates_format_of :first_name, with: NAME_REGEX, message: 'is invalid. Input full-width characters'
+
+  validates :first_name_kana, :last_name_kana, presence: true
+
+  KATAKANA_REGEX = /\A[ァ-ヴー]+\z/u.freeze
+
+  validates_format_of :last_name_kana, with: KATAKANA_REGEX, message: 'is invalid. Input full-width katakana characters' 
+  validates_format_of :first_name_kana, with: KATAKANA_REGEX, message: 'is invalid. Input full-width katakana characters' 
+  
 end
