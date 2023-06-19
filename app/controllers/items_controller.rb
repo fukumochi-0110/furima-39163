90e0cf3e5690be5570c348ad03010c212b0f6bc9
fukumochi-0110 <fukumoto08@gmail.com>
2023-06-19 21:04:14 +0900

    コントローラーの記述変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :same_action, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
-  before_action :same_action, only: [:show, :edit, :update, :destroy, :move_to_index]
+  
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -36,9 +37,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    return unless @item.destroy
-
-    redirect_to root_path
+    if current_user.id == @item.user.id
+      @item.destroy
+      redirect_to root_path
+    end
   end
 
   private
