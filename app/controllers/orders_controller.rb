e2c1fffb592febc500abed64afeb4b2f47f113ca
fukumochi-0110 <fukumoto08@gmail.com>
2023-06-30 22:57:36 +0900

    コントローラー作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,29 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def new
+    @order_address = OrderAddress.new
+  end
+  
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+
+      render :index, locals: {order_address: @order_address}
+    end
+
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture, :municipality, :street_address, :building_name, :phone_number, :prefecture_id).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 end
