a23af5816c37cef6da14f5182b2cb8fd1fa194e6
fukumochi-0110 <fukumoto08@gmail.com>
2023-07-04 22:46:43 +0900

    直接idを生成しないようにした

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -7,11 +7,7 @@ FactoryBot.define do
     street_address { '1111' }
     building_name { '東京ハイツ' }
     phone_number { '09012345678' }
-    user_id { 1 }
-    item_id { 1 }
     token {"tok_abcdefghijk00000000000000000"}
     
-    
-    association :order
   end
 end
