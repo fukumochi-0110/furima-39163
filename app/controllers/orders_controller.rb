852b2a93e77424602d8492b616ae64ae563e4709
fukumochi-0110 <fukumoto08@gmail.com>
2023-07-07 15:17:24 +0900

    他人の購入された商品にアクセスできないようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -51,8 +51,8 @@ class OrdersController < ApplicationController
 
   def purchased_item
     @order = Order.includes(:item).find_by(id: params[:item_id])
-
     return if @order.nil?
+    @item = @order.item
     if current_user.id != @item.user.id || @item.order.present?
       redirect_to root_path
     end
